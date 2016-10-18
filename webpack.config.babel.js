5104ef4220db443dc532ed917c44b4e8ecd21520
Stefan <stefanmirck@gmail.com>
2016-10-18 16:49:25 +0200

    Packaging the client app.

---
+++ webpack.config.babel.js
@@ -0,0 +1,16 @@
+export default {
+  entry: './app/index.js',
+  output: {
+    path: './public',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+      }
+    ]
+  }
+};
